3c37b51fb552aeba737b3282373bc82761c42e81
Sandy Pratama <sandy-pratama@jsgi.co.id>
2024-09-10 09:18:35 +0700

    Remove unused import

--- Form1.cs
+++ Form1.cs
@@ -1,14 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Charging_Monitoring
 {
@@ -23,40 +14,43 @@ namespace Charging_Monitoring
 
         private void InitializeBatteryMonitor()
         {
-            // Konfigurasi Timer
-            batteryCheckTimer = new Timer();
-            batteryCheckTimer.Interval = 10000; // Cek setiap 10 detik
+            // Timer Configuration
+            batteryCheckTimer = new Timer
+            {
+                Interval = 10000 // Check every 10 seconds
+            };
             batteryCheckTimer.Tick += BatteryCheckTimer_Tick;
             batteryCheckTimer.Start();
 
-            // Mulai dengan form tersembunyi
+            // Start with a hidden form
             this.WindowState = FormWindowState.Minimized;
             this.ShowInTaskbar = false;
         }
 
         private void BatteryCheckTimer_Tick(object sender, EventArgs e)
         {
-            // Mendapatkan status baterai
+            // Get battery status
             PowerStatus powerStatus = SystemInformation.PowerStatus;
             float batteryLifePercent = powerStatus.BatteryLifePercent * 100;
             BatteryChargeStatus chargeStatus = powerStatus.BatteryChargeStatus;
 
-            // Cek apakah baterai sedang di-charge dan telah mencapai 100%
+            // Check that the battery is charging and has reached 100%.
             if (chargeStatus.HasFlag(BatteryChargeStatus.Charging) && batteryLifePercent >= 50)
             {
-                // Menampilkan pesan peringatan
+                // Display a warning message
                 ShowBatteryFullNotification();
             }
         }
 
         private void ShowBatteryFullNotification()
         {
-            // Tampilkan notifikasi menggunakan NotifyIcon
-            //MessageBox.Show("Battery is fully charged. Please unplug the charger.", "Battery Full", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            // Show notifications using NotifyIcon
             notifyIcon1.BalloonTipTitle = "Battery Full";
             notifyIcon1.BalloonTipText = "Battery is fully charged. Please unplug the charger.";
             notifyIcon1.ShowBalloonTip(5000);
 
+            // MessageBox.Show("Battery is fully charged. Please unplug the charger.", "Battery Full", MessageBoxButtons.OK, MessageBoxIcon.Warning);
         }
     }
 }
